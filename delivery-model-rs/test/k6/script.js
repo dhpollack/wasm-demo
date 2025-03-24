9be1d9ce0b1e8f2dff565b3c1788e19357af7bb5
David Pollack <david@da3.net>
2025-03-24 18:19:54 +0100

    Simple k6 test

--- delivery-model-rs/test/k6/script.js
+++ delivery-model-rs/test/k6/script.js
@@ -3,11 +3,33 @@ import { sleep, check } from 'k6';
 
 export const options = {
   vus: 10,
-  duration: '30s',
+  duration: '10s',
 };
 
+export function generateRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function generateRandomFloat(min, max) {
+  return Math.random() * (max - min + 1) + min;
+}
+
 export default function() {
-  let res = http.get('https://quickpizza.grafana.com');
+  const url = "http://0.0.0.0:8000/"
+  let payload = JSON.stringify({
+    age: generateRandomFloat(20.0, 42.0),
+    rating: generateRandomFloat(1.0, 5.0),
+    dist: generateRandomFloat(10.0, 30.0),
+    order_type: generateRandomInt(1, 4),
+    vehicle_type: generateRandomInt(1, 4),
+  });
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  let res = http.post(url, payload, params);
   check(res, { "status is 200": (res) => res.status === 200 });
-  sleep(1);
+  sleep(0.2);
 }
